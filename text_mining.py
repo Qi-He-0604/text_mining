a256ac1899e4368bff951075d7ad661eee052a61
Stella He <58181809+Qi-He-0604@users.noreply.github.com>
2020-10-10 15:14:59 -0400

    Update text_mining.py

--- text_mining.py
+++ text_mining.py
@@ -7,11 +7,13 @@ f.close()
 text = text.lower()
 new_text = re.sub('[^ a-zA-Z0-9]', '', text)
 
+# count the number of 4,5,6 letter words
 text_count = list(map(len, new_text.split()))
 print('Number of 4 letter words: ' + str(text_count.count(4)))
 print('Number of 5 letter words: ' + str(text_count.count(5)))
 print('Number of 6 letter words: ' + str(text_count.count(6)))
 
+# count the number of a user given word
 input_word = input('Please enter a word? ').lower()
 print('input_word: ' + input_word)
 count = new_text.count(input_word)
